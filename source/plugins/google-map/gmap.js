37a2689ee39b3df593e67ff817252bc59edbe8eb
Md Sojon Islam <sojonatmail@gmail.com>
2021-01-02 16:51:33 +0600

    source files & README modified

--- source/plugins/google-map/gmap.js
+++ source/plugins/google-map/gmap.js
@@ -6,98 +6,219 @@ function initialize() {
   var longitude = $('#map_canvas').attr('data-longitude');
   var nottingham = new google.maps.LatLng(latitude, longitude);
   var style = [{
+      "featureType": "administrative.locality",
+      "elementType": "all",
+      "stylers": [{
+          "hue": "#2c2e33"
+        },
+        {
+          "saturation": 7
+        },
+        {
+          "lightness": 19
+        },
+        {
+          "visibility": "on"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.locality",
+      "elementType": "labels.text",
+      "stylers": [{
+          "visibility": "on"
+        },
+        {
+          "saturation": "-3"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.locality",
+      "elementType": "labels.text.fill",
+      "stylers": [{
+        "color": "#282a00"
+      }]
+    },
+    {
       "featureType": "landscape",
+      "elementType": "all",
       "stylers": [{
-          "hue": "#FFAD00"
+          "hue": "#ffffff"
         },
         {
-          "saturation": 50.2
+          "saturation": -100
         },
         {
-          "lightness": -34.8
+          "lightness": 100
         },
         {
-          "gamma": 1
-        }
-      ]
-    },
-    {
-      "featureType": "road.highway",
-      "stylers": [{
-          "hue": "#FFAD00"
-        },
-        {
-          "saturation": -19.8
-        },
-        {
-          "lightness": -1.8
-        },
-        {
-          "gamma": 1
-        }
-      ]
-    },
-    {
-      "featureType": "road.arterial",
-      "stylers": [{
-          "hue": "#FFAD00"
-        },
-        {
-          "saturation": 72.4
-        },
-        {
-          "lightness": -32.6
-        },
-        {
-          "gamma": 1
-        }
-      ]
-    },
-    {
-      "featureType": "road.local",
-      "stylers": [{
-          "hue": "#FFAD00"
-        },
-        {
-          "saturation": 74.4
-        },
-        {
-          "lightness": -18
-        },
-        {
-          "gamma": 1
-        }
-      ]
-    },
-    {
-      "featureType": "water",
-      "stylers": [{
-          "hue": "#00FFA6"
-        },
-        {
-          "saturation": -63.2
-        },
-        {
-          "lightness": 38
-        },
-        {
-          "gamma": 1
+          "visibility": "simplified"
         }
       ]
     },
     {
       "featureType": "poi",
+      "elementType": "all",
       "stylers": [{
-          "hue": "#FFC300"
+          "hue": "#ffffff"
         },
         {
-          "saturation": 54.2
+          "saturation": -100
         },
         {
-          "lightness": -14.4
+          "lightness": 100
         },
         {
-          "gamma": 1
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.school",
+      "elementType": "geometry.fill",
+      "stylers": [{
+          "color": "#f39247"
+        },
+        {
+          "saturation": "0"
+        },
+        {
+          "visibility": "on"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry",
+      "stylers": [{
+          "hue": "#ffb600"
+        },
+        {
+          "saturation": "100"
+        },
+        {
+          "lightness": 31
+        },
+        {
+          "visibility": "simplified"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry.stroke",
+      "stylers": [{
+          "color": "#ffb600"
+        },
+        {
+          "saturation": "0"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels",
+      "stylers": [{
+          "hue": "#008eff"
+        },
+        {
+          "saturation": -93
+        },
+        {
+          "lightness": 31
+        },
+        {
+          "visibility": "on"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "geometry.stroke",
+      "stylers": [{
+          "visibility": "on"
+        },
+        {
+          "color": "#f3dbc8"
+        },
+        {
+          "saturation": "0"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "labels",
+      "stylers": [{
+          "hue": "#bbc0c4"
+        },
+        {
+          "saturation": -93
+        },
+        {
+          "lightness": -2
+        },
+        {
+          "visibility": "simplified"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "labels.text",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "geometry",
+      "stylers": [{
+          "hue": "#e9ebed"
+        },
+        {
+          "saturation": -90
+        },
+        {
+          "lightness": -8
+        },
+        {
+          "visibility": "simplified"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "elementType": "all",
+      "stylers": [{
+          "hue": "#e9ebed"
+        },
+        {
+          "saturation": 10
+        },
+        {
+          "lightness": 69
+        },
+        {
+          "visibility": "on"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "all",
+      "stylers": [{
+          "hue": "#e9ebed"
+        },
+        {
+          "saturation": -78
+        },
+        {
+          "lightness": 67
+        },
+        {
+          "visibility": "simplified"
         }
       ]
     }
@@ -124,7 +245,7 @@ function initialize() {
   map.mapTypes.set('grey', mapType);
   map.setMapTypeId('grey');
   var marker_image = 'plugins/google-map/images/marker.png';
-  var pinIcon = new google.maps.MarkerImage(marker_image, null, null, null, new google.maps.Size(37, 55));
+  var pinIcon = new google.maps.MarkerImage(marker_image, null, null, null, new google.maps.Size(56, 55));
   marker = new google.maps.Marker({
     position: nottingham,
     map: map,
